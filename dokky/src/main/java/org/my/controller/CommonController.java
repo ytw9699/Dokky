72699a2247677d3388f0b5650eb7f278b7a9bed4
home_champ <ytw9699@naver.com>
2019-07-14 22:11:31 +0900

    불필요파일 정리 통합

--- dokky/src/main/java/org/my/controller/CommonController.java
+++ dokky/src/main/java/org/my/controller/CommonController.java
@@ -1,24 +1,22 @@
 package org.my.controller;
 	import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-
-import org.my.domain.MemberVO;
-import org.my.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import lombok.Setter;
-import lombok.extern.log4j.Log4j;
+	import java.util.Locale;
+	import org.my.domain.MemberVO;
+	import org.my.service.MemberService;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.http.HttpStatus;
+	import org.springframework.http.ResponseEntity;
+	import org.springframework.security.core.Authentication;
+	import org.springframework.security.crypto.password.PasswordEncoder;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestMethod;
+	import org.springframework.web.bind.annotation.ResponseBody;
+	import lombok.Setter;
+	import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j 
@@ -29,6 +27,17 @@ public class CommonController {
 	
 	@Setter(onMethod_ = @Autowired)
 	private PasswordEncoder pwencoder;
+	
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String main(Model model) {
+		
+		return "main";
+	}
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "main";
+	}
 	
 	@GetMapping("/adminError")
 	public String adminError(Model model) {
