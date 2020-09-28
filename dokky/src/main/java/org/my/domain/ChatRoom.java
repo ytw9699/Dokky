d32df38323e083b15fd1aba085d59b56fc031daa
home_champ <ytw9699@naver.com>
2020-09-28 11:34:44 +0900

    채팅룸 객체

---
+++ dokky/src/main/java/org/my/domain/ChatRoom.java
@@ -0,0 +1,97 @@
+package org.my.domain;
+	import java.io.IOException;
+	import java.util.HashSet;
+	import org.springframework.web.socket.TextMessage;
+	import org.springframework.web.socket.WebSocketSession;
+	import com.fasterxml.jackson.databind.ObjectMapper;
+	import lombok.Data;
+	import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Data
+public class ChatRoom {
+	
+    private HashSet<WebSocketSession> sessionsSet = new HashSet<>();//채팅방에 접속해있는 세션들
+    
+    
+    public void handleMessage(WebSocketSession session, ChatMessage chatMessage) throws IOException{
+    	
+    	log.info("handleMessage");
+    	
+        if(chatMessage.getType() == ChatMessageType.OPEN){
+        	
+        	log.info("add session = "+session);
+        	
+        	sessionsSet.add(session);
+        	
+        }else if(chatMessage.getType() == ChatMessageType.CLOSED){
+        	
+        	log.info("remove session = "+session);
+        	
+    		sessionsSet.remove(session);
+            
+        }else if(chatMessage.getType() == ChatMessageType.LEAVE){
+        	
+        	log.info("remove session = "+session);
+        	
+        	sessionsSet.remove(session);
+            
+            send(chatMessage);
+        
+        }else if(chatMessage.getType() == ChatMessageType.INVITE){
+        	
+	    }else if(chatMessage.getType() == ChatMessageType.OUT){
+	    	
+	    	log.info("remove session = "+session);
+        	
+    		sessionsSet.remove(session);
+        
+        }else if(chatMessage.getType() == ChatMessageType.CHAT){
+        	
+        	send(chatMessage);
+        }
+    }
+
+    private void send(ChatMessage chatMessage) throws IOException {
+    	
+    	String customMessage = null;
+    	
+    	if(chatMessage.getType() == ChatMessageType.CHAT){
+        	
+        	customMessage = 	"{\"chatRoomNum\":\""+chatMessage.getChatRoomNum()+
+    								"\", \"chat_writerNick\":\""+chatMessage.getChat_writerNick()+
+    								"\", \"chat_writerId\":\""+chatMessage.getChat_writerId()+
+    								"\", \"message\":\""+chatMessage.getMessage()+
+    								"\", \"type\":\""+chatMessage.getType()+
+    								"\", \"regDate\":\""+chatMessage.getRegDate().getTime()+"\"}";
+        	
+        }else if(chatMessage.getType() == ChatMessageType.LEAVE){
+        	
+        	customMessage = 	"{\"message\":\""+chatMessage.getMessage()+
+					"\", \"type\":\""+chatMessage.getType()+"\"}";
+        }
+    	
+    	TextMessage textMessage = new TextMessage(customMessage); 
+        
+    	for(WebSocketSession session : sessionsSet){
+        	
+        	log.info("send message");
+    		
+    		session.sendMessage(textMessage);//채팅방 모든 사람에게 메시지 보내는것
+        }
+
+        /*for(WebSocketSession session : sessionsSet){
+        	
+        	if(session.isOpen()) {
+        		
+        		log.info("send message ="+session);
+        		session.sendMessage(textMessage);//채팅방 모든 사람에게 메시지 보내는것
+        	
+        	}else{
+        		
+        		log.info("remove session ="+session);
+        		sessionsSet.remove(session);
+        	}
+        }*/
+    }
+}
