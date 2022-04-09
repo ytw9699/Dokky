8913c51ff7760d741bd84a4132a6f58b40decac1
champ_home <ytw9699@naver.com>
2022-04-09 18:49:39 +0900

    s3 버킷 이름 변경

--- dokky/src/main/java/org/my/s3/myS3Util.java
+++ dokky/src/main/java/org/my/s3/myS3Util.java
@@ -40,7 +40,7 @@ import java.io.OutputStream;
 @Component
 public class myS3Util { 
 	
-	private static final String bucket_name = "dokky-bucket";//자신의 S3 버킷 이름을 입력하세요
+	private static final String bucket_name = "dokky2-bucket";//자신의 S3 버킷 이름을 입력하세요
 	private static final String ACCESS_KEY = "";//자신의 S3 ACCESS_KEY를 입력하세요
 	private static final String SECRET_KEY = "";//자신의 S3 SECRET_KEY를 입력하세요
 	
