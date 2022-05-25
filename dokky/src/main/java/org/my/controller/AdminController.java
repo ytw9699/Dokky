dcb6d8439ed00c2dc554b8bbf9d7f79455928ce0
champ_home <ytw9699@naver.com>
2022-05-26 08:24:20 +0900

    1. 글쓰기 권한은 아예 삭제, 일반유저가 아니라면 글쓰기 못하게 처리할것 2. 일반유저인지, 일반관리자인지, 슈퍼관리자인지 권한을 3가지로 나눠서 처리, 3. 요청 url 을 admin과 superAdmin 구분

--- dokky/src/main/java/org/my/controller/AdminController.java
+++ dokky/src/main/java/org/my/controller/AdminController.java
@@ -1,3 +1,6 @@
+/*
+- 마지막 업데이트 2022-05-25
+*/
 package org.my.controller;
 	import org.my.domain.Criteria;
 	import org.my.domain.PageDTO;
@@ -5,7 +8,6 @@ package org.my.controller;
 	import org.my.domain.commonVO;
 	import org.my.service.AdminService;
 	import org.my.service.MypageService;
-	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.http.HttpStatus;
 	import org.springframework.http.ResponseEntity;
 	import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,31 +15,28 @@ package org.my.controller;
 	import org.springframework.ui.Model;
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.PostMapping;
 	import org.springframework.web.bind.annotation.RequestBody;
 	import org.springframework.web.bind.annotation.RequestMapping;
 	import org.springframework.web.bind.annotation.RequestMethod;
 	import org.springframework.web.bind.annotation.RequestParam;
 	import org.springframework.web.bind.annotation.ResponseBody;
-	import lombok.Setter;
+	import lombok.RequiredArgsConstructor;
 	import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-@RequestMapping("/admin/*")
-@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_SUPER')")
+@RequiredArgsConstructor
 public class AdminController {
 	
-	@Setter(onMethod_ = @Autowired)
-	private AdminService adminService;
+	private final AdminService adminService;
+	private final MypageService mypageService;
 	
-	@Setter(onMethod_ = @Autowired)
-	private MypageService mypageService;
-	
-	@GetMapping("userList")//계정관리 회원리스트 가져오기
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/admin/userList")
 	public String userList(Criteria cri, Model model) {
 	    
 		log.info("/admin/userList");
-		
 		log.info("cri"+cri);
 		
 		model.addAttribute("userList", adminService.getUserList(cri));
@@ -49,10 +48,12 @@ public class AdminController {
 		return "admin/userList"; 
 	}
 	
-	@GetMapping("cashRequestList")//결제관리 충전,환전 요청내역 리스트가져오기
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/admin/cashRequestList")
 	public String cashRequestList(Criteria cri, Model model) {
 		
 		log.info("admin/cashRequestList");
+		log.info(cri);
 		
 		model.addAttribute("cashRequestList", adminService.getCashRequestList(cri));
 		
@@ -63,11 +64,11 @@ public class AdminController {
 		return "admin/cashRequestList"; 
 	}
 	
-	@GetMapping("userReportList")//신고리스트 가져오기
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/admin/userReportList")
 	public String userReportList(Criteria cri, Model model) {
 		
 		log.info("admin/userReportList");
-		
 		log.info(cri);
 		
 		model.addAttribute("userReportList", adminService.getUserReportList(cri));
@@ -79,7 +80,8 @@ public class AdminController {
 		return "admin/userReportList"; 
 	}
 	
-	@GetMapping("/userForm")  //관리자가 회원정보가져오기
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/admin/userForm")
 	public String userForm(@RequestParam("userId") String userId, Model model) {
 		
 		log.info("admin/userForm");
@@ -89,81 +91,93 @@ public class AdminController {
 		return "admin/userForm";
 	} 
 	
-	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH },
-			value = "roleLimit/{userId}", produces = "text/plain; charset=UTF-8")
-	@ResponseBody //모든 글쓰기 제한
-	public ResponseEntity<String> updateRoleLimit(@PathVariable("userId") String userId, @RequestBody alarmVO vo) {
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping(value = "/admin/limitLogin/{userId}", produces = "text/plain; charset=UTF-8")
+	@ResponseBody
+	public ResponseEntity<String> limitLogin(@PathVariable("userId") String userId, @RequestBody alarmVO vo) {
 	
-		log.info("admin/roleLimit");
-		log.info("vo"+vo);
+		log.info("admin/limitLogin");
 		log.info("userId...="+userId);
+		log.info("vo"+vo);
 		
-		return adminService.updateRoleLimit(userId, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+		return adminService.limitLogin(userId, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH },
-			value = "roleStop/{userId}", produces = "text/plain; charset=UTF-8")
-	@ResponseBody //접속 제한
-	public ResponseEntity<String> updateRoleStop(@PathVariable("userId") String userId, @RequestBody alarmVO vo) {
-		
-		log.info("admin/roleStop");
-		log.info("vo"+vo);
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping(value = "/admin/permitLogin/{userId}", produces = "text/plain; charset=UTF-8")
+	@ResponseBody
+	public ResponseEntity<String> permitLogin(@PathVariable("userId") String userId , @RequestBody alarmVO vo) {
+	
+		log.info("admin/permitLogin");
 		log.info("userId...="+userId);
+		log.info("vo"+vo);
+	
+		return adminService.permitLogin(userId, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+			: new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@PreAuthorize("hasRole('ROLE_SUPER')")
+	@PostMapping(value = "superAdmin/createRoleAdmin/{userId}/{role}", produces = "text/plain; charset=UTF-8")
+	@ResponseBody
+	public ResponseEntity<String> createRoleAdmin(@PathVariable("userId") String userId,
+					@PathVariable("role") String role, @RequestBody alarmVO vo) {
 		
-		return adminService.updateRoleStop(userId, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+		return adminService.insertRole(userId, role, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH },
-			value = "roleUser/{userId}", produces = "text/plain; charset=UTF-8")
-	@ResponseBody //계정 복구
-	public ResponseEntity<String> updateRoleUser(@PathVariable("userId") String userId , @RequestBody alarmVO vo) {
+	@PreAuthorize("hasRole('ROLE_SUPER')")
+	@PostMapping(value = "superAdmin/deleteRoleAdmin/{userId}/{role}", produces = "text/plain; charset=UTF-8")
+	@ResponseBody
+	public ResponseEntity<String> deleteRoleAdmin(@PathVariable("userId") String userId,
+					@PathVariable("role") String role, @RequestBody alarmVO vo) {
 		
-		log.info("admin/roleUser");
-		log.info("vo"+vo);
-		log.info("userId...="+userId);
-		
-		return adminService.updateRoleUser(userId, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+		return adminService.deleteRole(userId, role, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	
-	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH },
-			value = "roleAdmin/{userId}", produces = "text/plain; charset=UTF-8")
-	@ResponseBody //관리자로 계정 변경
-	public ResponseEntity<String> updateRoleAdmin(@PathVariable("userId") String userId, @RequestBody alarmVO vo) {
-	
-		log.info("admin/roleAdmin");
-		log.info("vo"+vo);
-		log.info("userId...="+userId);
-	
-		return adminService.updateRoleAdmin(userId, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping(value = "/admin/createRoleUser/{userId}/{role}", produces = "text/plain; charset=UTF-8")
+	@ResponseBody
+	public ResponseEntity<String> createRoleUser(@PathVariable("userId") String userId,
+					@PathVariable("role") String role, @RequestBody alarmVO vo) {
+		
+		return adminService.insertRole(userId, role, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
- 	@GetMapping("/userCashHistory") 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping(value = "/admin/deleteRoleUser/{userId}/{role}", produces = "text/plain; charset=UTF-8")
+	@ResponseBody
+	public ResponseEntity<String> deleteRoleUser(@PathVariable("userId") String userId,
+					@PathVariable("role") String role, @RequestBody alarmVO vo) {
+		
+		return adminService.deleteRole(userId, role, vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+ 	@GetMapping("/admin/userCashHistory") 
 	public String userCashHistory(Criteria cri, Model model) {
-		
-		log.info("getUserCashHistoryCount");
-		
-		int total = mypageService.getMyCashHistoryCount(cri.getUserId());
 		
 		log.info("/userCashHistory");
 		
-		model.addAttribute("userCashHistory", mypageService.getMyCashHistory(cri));
+		int total = mypageService.getMyCashHistoryCount(cri.getUserId());
+		log.info("getUserCashHistoryCount");
 		
-		log.info("pageMaker");
+		model.addAttribute("userCashHistory", mypageService.getMyCashHistory(cri));
 		
 		model.addAttribute("pageMaker", new PageDTO(cri, total));
 		
 		return "admin/userCashHistory"; 
 	}
 	
-	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH },
-			value = "/approveCash", consumes = "application/json", produces = "text/plain; charset=UTF-8")
-		@ResponseBody
-		public ResponseEntity<String> approveCash(@RequestBody commonVO vo) {//캐시 환전 및 충전 하기
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@RequestMapping(method = { RequestMethod.PUT, RequestMethod.PATCH }, 
+					value = "/admin/approveCash", consumes = "application/json", produces = "text/plain; charset=UTF-8")
+	@ResponseBody
+	public ResponseEntity<String> approveCash(@RequestBody commonVO vo){
 		
 		log.info("/approveCash");
 		log.info("commonVO...="+vo);
