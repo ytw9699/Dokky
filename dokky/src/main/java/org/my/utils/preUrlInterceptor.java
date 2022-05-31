dbab6ec9d360b95ee33deee591e928ec7aa807ee
champ_home <ytw9699@naver.com>
2022-05-31 21:38:22 +0900

    인증이 되지 않은 사용자에 한하여 수행하게 인터셉터  변경

--- dokky/src/main/java/org/my/utils/preUrlInterceptor.java
+++ dokky/src/main/java/org/my/utils/preUrlInterceptor.java
@@ -1,8 +1,12 @@
+/*마지막 업데이트 2022-05-31
+1. 로그인후 이전 페이지로 이동하기 위해 url을 세션에 저장
+2. 인증이 되지 않은 사용자에 한하여 수행*/
 package org.my.utils;
 	import java.util.Enumeration;
 	import javax.servlet.http.HttpServletRequest;
 	import javax.servlet.http.HttpServletResponse;
 	import javax.servlet.http.HttpSession;
+	import org.springframework.security.core.context.SecurityContextHolder;
 	import org.springframework.web.servlet.ModelAndView;
 	import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 	import lombok.extern.log4j.Log4j;
@@ -11,45 +15,44 @@ package org.my.utils;
 public class preUrlInterceptor extends HandlerInterceptorAdapter{
 		
 	@Override
-	public boolean preHandle(HttpServletRequest request, //오버라이드 요청을 가로채서 컨트롤러까지 일단 요청 못들어감
-			HttpServletResponse response, Object handler) throws Exception {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		
 		log.info("인터셉터 시작");
 		
-		HttpSession session = request.getSession();
+		if(SecurityContextHolder.getContext().getAuthentication() == null || SecurityContextHolder.getContext().getAuthentication().getPrincipal() == "anonymousUser"){
 		
-		String preURI = request.getRequestURI();
-		
-		//if(!(preURI.equals("/socialLogin") || preURI.equals("/superAdminLogin"))){
+			HttpSession session = request.getSession();
 			
-        Enumeration<String> e = request.getParameterNames();
-            
-        if(e.hasMoreElements()) {
-        	   preURI = preURI +"?";
-        }
-           
-        while(e.hasMoreElements()){
-    	   
-    	   String nextElement = e.nextElement();
-    	   preURI = preURI + nextElement + "=";
-    	   preURI = preURI + request.getParameter(nextElement);
-    	   
-    	   if(e.hasMoreElements()) {
-        	   preURI = preURI +"&";
-           }
-        }
+			String preURI = request.getRequestURI();
+				
+	        Enumeration<String> e = request.getParameterNames();
+	            
+	        if(e.hasMoreElements()) {
+	        	   preURI = preURI +"?";
+	        }
+	           
+	        while(e.hasMoreElements()){
+	    	   
+	    	   String nextElement = e.nextElement();
+	    	   preURI = preURI + nextElement + "=";
+	    	   preURI = preURI + request.getParameter(nextElement);
+	    	   
+	    	   if(e.hasMoreElements()) {
+	        	   preURI = preURI +"&";
+	           }
+	        }
+			
+			session.setAttribute("preUrl", preURI);
+		}
 		
-		session.setAttribute("preUrl", preURI);
-		
-		log.info("preURI="+preURI);
 		log.info("인터셉터 끝");
-	
+		
 		return true;
 	}
 	
-	@Override
-	public void postHandle(HttpServletRequest request,//클라이언트의 요청을 가로채되 컨트롤러 처리 후 하는작업들
-			HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
+	@Override//클라이언트의 요청을 가로채되 컨트롤러 처리 후 하는작업들
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		 
+		log.info("postHandle ");
 	}
 }
